a5e42e7ebb5b102ec9ce0728d62f3a7e83191949
jsrikar27 <138281705+jsrikar27@users.noreply.github.com>
2023-07-01 15:28:03 +0530

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -34,7 +34,7 @@ public class Hook extends BaseUtil{
     @Before
     public void InitializeTest(Scenario scenario) {
         base.scenarioDef = base.features.createNode(scenario.getName());
-        System.setProperty("webdriver.http.factory", "jdk-http-client");
+     //   System.setProperty("webdriver.http.factory", "jdk-http-client");
      //   System.setProperty("webdriver.edge.verboseLogging", "true");
          System.setProperty("webdriver.edge.driver", "C:\\driver\\msedgedriver.exe");
          WebDriver driver = new EdgeDriver();
